8ec4d7bbf0afd1ab1ebe27f0b3af225f3d52dd72
Bui Minh Thu <buiminhthuk55@gmail.com>
2016-04-11 00:22:40 +0900

    devide to basic_services

--- app/controllers/hotel_services_controller.rb
+++ app/controllers/hotel_services_controller.rb
@@ -1,44 +1,6 @@
 class HotelServicesController < ApplicationController
   soap_service namespace: "urn:hotel_ws", wsdl_style: "document"
 
-  soap_action "add_new_hotel",
-    args: {code: :string, name: :string, star: :integer,
-      city: :string, country: :string, address: :string,
-      website: :string, phone: :string, totalRooms: :integer,
-      cost: :integer},
-    return: :string
-
-  def add_new_hotel
-    standarlize_params
-    hotel = Hotel.new params
-    messages = hotel.save ? I18n.t("action.success") : hotel.errors.full_messages
-    render soap: messages
-  end
-
-  soap_action "is_existed_hotel",
-    args: {code: :string, name: :string},
-    return: :boolean
-
-  def is_existed_hotel
-    hotel = Hotel.find_by code: params[:code] if params[:code].present?
-    if !hotel.present? && params[:name].present?
-      hotel = Hotel.find_by name: params[:name] unless hotel.present?
-    end
-    render soap: hotel.present?
-  end
-
-  soap_action "find_hotels_by_city",
-    args: {cityName: :string},
-    return: :string
-
-  def find_hotels_by_city
-    if params[:cityName].present?
-      render soap: Hotel.where(city: params[:cityName]).to_json
-    else
-      render soap: I18n.t("errors.param_not_present", param: "cityName")
-    end
-  end
-
   soap_action "add_new_constract",
     args: {add_new_constract_request: {
       hotelId: :integer, customerIdNumber: :string,
@@ -66,13 +28,6 @@ class HotelServicesController < ApplicationController
     else
       render soap: {result: I18n.t("errors.param_not_present", param: "hotelCode")}
     end
-  end
-
-  soap_action "get_all_hotels",
-    return: :string
-
-  def get_all_hotels
-    render soap: Hotel.all.to_json
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   wash_out :hotel_services
+  wash_out :hotel_basic_services
 end

89b8f6e455e620c546f1dc4bd28e63b11ea0679f
Bui Minh Thu <buiminhthuk55@gmail.com>
2016-04-18 01:06:41 +0900

    add new constract

--- app/controllers/hotel_services_controller.rb
+++ app/controllers/hotel_services_controller.rb
@@ -14,4 +14,39 @@ class HotelServicesController < ApplicationController
     hotels = Hotel.where condition
     render soap: {result: hotels.empty? ? 0 : 1}
   end
+
+  soap_action "add_new_constract",
+    args: {add_new_constract_request: {
+      hotelId: :integer, customerIdNumber: :string,
+      companyName: :string, companyPhone: :string,
+      companyAddress: :string, bookingRooms: :integer,
+      checkInDate: :string, checkOutDate: :string}},
+    return: {result: :integer}
+
+  def add_new_constract
+    params = get_constract_params
+
+    constract = Constract.new standarlize_params params
+    messages = constract.save ? constract.id : -1
+    render soap: {result: messages}
+  end
+
+  private
+  def standarlize_params params
+    params.keys.each do |key|
+      unless key.to_s == key.to_s.underscore
+        params[key.to_s.underscore.to_sym] = params[key]
+        params.delete key
+      end
+    end
+    params
+  end
+
+  def get_constract_params
+    check_in_date = params[:add_new_constract_request][:checkInDate].to_date
+    check_out_date = params[:add_new_constract_request][:checkOutDate].to_s
+    params[:add_new_constract_request][:checkInDate] = check_in_date
+    params[:add_new_constract_request][:checkOutDate] = check_out_date
+    params[:add_new_constract_request]
+  end
 end
